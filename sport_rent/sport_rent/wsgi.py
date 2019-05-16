e76d2c86c55295c099aeb871dadad67b42bb40be
vertolol <pingvin000000@rambler.ru>
2019-05-16 10:40:10 +0400

    add settings

--- sport_rent/sport_rent/wsgi.py
+++ sport_rent/sport_rent/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sport_rent.settings.devs')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sport_rent.settings')
 
 application = get_wsgi_application()
